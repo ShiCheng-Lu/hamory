0da8795a1ce501377f19914eb1fcaec6554d0fdd
ShiCheng Lu <shichenglu0708@gmail.com>
2025-03-29 22:36:38 -0400

    get last available handle address on WRITE 1 111 xxxx

--- object_cell.v
+++ object_cell.v
@@ -15,7 +15,17 @@ else if read/write, translate to addr
 
 unit commands:
 read from handle 0: return the next available handle address
+
+1 00000
+
 */
+
+`define NOP 0
+`define READ 1
+`define WRITE 2
+
+`define H_ADDR(handle_id, address) (1 << 15 + handle_id << 12 + address) 
+`define H_OP(handle_id) ((1 << 15) + ('b111 << 12) + handle_id)
 
 /*
 TODO: 
@@ -33,51 +43,111 @@ this module translates an object handle into the real address
 */
 module object_cell #(
     parameter W = 16,
-    parameter [6:0] id = 0,
+    parameter HW = 3,
+    parameter [HW-1:0] id = 0,
     parameter [7:0] map = 0
 ) (
-    input [W-1:0] i_address,
-    inout [W-1:0] io_data,
     input i_clock,
-    output wire [W-1:0] o_address,
+    input [W-1:0] i_address,
+    input [W-1:0] i_data,
+    inout [W-1:0] o_data,
+    output wire [W-HW-1:0] o_offset,
+    input write_to_map,
+    input get_available_id,
+    input write_valid
 );
-    reg [7:0] mapped_address = map;
+    reg [W-HW-1:0] mapped_address = map;
     reg valid = 0;
-    reg [W-1:0] candidate_output;
+    reg [W-HW-1:0] candidate_output;
     // at rising edge, compare if the address matches the handle, 
     // output the translated address if matches
     always @(posedge i_clock) begin
         // a == b can be implmented with ~|(a ^ b) equiv. nor(bits of xor(a, b))
-        if (i_address[W-1] & (i_address[W-2:W-8] == id)) begin
+        if (i_address[W-1] & (i_address[W-2:W-HW-2] == id)) begin
             candidate_output <= {mapped_address, i_address[W-9:0]};
         end else begin
             // if doesn't match, weakly output 0
             candidate_output <= 0;
         end
+
+        // handling writing to validness
+        if (write_valid) begin
+            valid = i_data;
+        end
     end
     // drive o_address with strong 1s and weak 0s
-    assign (strong1, weak0) o_address = candidate_output;
+    assign (strong1, weak0) o_offset = candidate_output;
+    
+
+    // handling getting available ids
+    assign outputs_id[HW-1] = get_available_id & !valid;
+    // logic to output the id if this handle is invalid
+    wire [HW-1:0] outputs_id;
+    assign (strong1, weak0) o_data[HW-1] = outputs_id[HW-1] & id[HW-1];
+    generate
+        for (genvar i = 0; i < HW - 1; i = i + 1) begin
+            assign (strong1, weak0) outputs_id[i] = outputs_id[i + 1] & o_data[i + 1] == id[i + 1];
+            assign (strong1, weak0) o_data[i] = id[i] & outputs_id[i];
+        end
+    endgenerate
+endmodule
+
+/*
+
+*/
+module handle_handler #(
+    parameter W = 16,
+    parameter HW = 3
+) (
+    input i_clock,
+    input [2:0] i_op,
+    input [W-1:0] i_address,
+    input [W-1:0] i_data,
+    output [2:0] o_op,
+    output [W-1:0] o_address,
+    output [W-1:0] o_data
+);
+    wire [W-HW-1:0] offset;
+
+    reg write_to_map;
+    wire get_available_id;
+    reg write_valid;
+
+    object_cell #(.W(W), .HW(HW), .id('b101), .map(5)) c1 (i_clock, i_address, i_data, o_data, offset, write_to_map, get_available_id, write_valid);
+    object_cell #(.W(W), .HW(HW), .id('b010), .map(7)) c2 (i_clock, i_address, i_data, o_data, offset, write_to_map, get_available_id, write_valid);
+
+    assign o_address = i_address + offset;
+    assign get_available_id = (i_op == `READ) & (&i_address[W-2:W-HW-1]);
 endmodule
 
 // test bench
 module object_cell_tb;
-  reg [15:0] data = 0;
-  initial begin
-    # 1 data = 'h8066;
-    # 4 data = 'h812c;
-    # 4 data = 'h802c;
-    # 200 $stop;
-  end
-  /* Make a regular pulsing clock. */
-  reg clk = 0;
-  always #1 clk = !clk;
+    reg [2:0] op = 0; // 0 for no-op, 1 for read, 2 for write
+    reg [15:0] addr = 0;
+    reg [15:0] data = 0;
 
-  wire [15:0] result;
+    initial begin
+        # 1 addr = `H_OP(0); data = 1; op = `READ;
+        # 1 addr = 'b0; data = 1; op = `NOP;
+        # 1 addr = 'h8063; data = 1; op = `NOP;
+        # 1 addr = 'h8064; data = 1; op = `NOP;
+        # 1 addr = 'h812c; data = 1; op = `NOP;
+        # 1 addr = 'h802c; data = 1; op = `NOP;
+        # 200 $stop;
+    end
+    /* Make a regular pulsing clock. */
+    reg clk = 0;
+    always #1 clk = !clk;
 
-  object_cell #(.id(0), .map(5)) c1 (data, clk, result);
-  object_cell #(.id(1), .map(7)) c2 (data, clk, result);
+    wire [2:0] o_op;
+    wire [15:0] o_address;
+    wire [15:0] o_data;
 
-  initial
-     $monitor("At time %t, data = %h, value = %h (%0d)",
-              $time, data, result, result);
+    assign (strong1, weak0) o_data = 0;
+
+    handle_handler s(clk, op, addr, data, o_op, o_address, o_data);
+
+    initial
+        $monitor("At time %t | inputs: %d, %h, %h | outputs: %d, %h, %h, ",
+                $time, op, addr, data, o_op, o_address, o_data);
 endmodule
